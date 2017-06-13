d0c0596ef7676b0f4dfca36edab0af0d09047e9b
Wladimir d'Harcourt <wladimir@dalou.co.uk>
2017-06-13 15:42:37 +0200

    Delete user routes
    Delete user route and use resources for events

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'events/index'
-
-  get 'events/show'
-
-  get 'events/new'
-
-  get 'events/create'
-
-  get 'events/edit'
-
-  get 'events/update'
-
-  get 'events/destroy'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
+  resources :events
+  
   devise_for :users
+  
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
