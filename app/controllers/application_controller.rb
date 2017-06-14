e2c186a062eb1ff9b889510daf5481ba453477a5
WladStan <wladharcourt@gmail.com>
2017-06-14 12:38:40 +0200

    required log in action before creating or booking events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :description])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :description])
-
   end
+
 end
