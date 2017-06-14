4354c72c19691398055122578ead13b93a69cc3a
Weronika Tomaszewska <vikariusu@gmail.com>
2017-06-14 17:06:59 +0200

    added files html, routes, css

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     get 'book', to: "events#book"
   end
 
-  devise_for :users
+  devise_for :users do
+    get 'dashboard', to: "users#dashboard"
+  end
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
