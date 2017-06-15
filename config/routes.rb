b0c4d4ce5f5595b5b7695fb42af6fb2fe589c4c7
Weronika Tomaszewska <vikariusu@gmail.com>
2017-06-15 12:54:49 +0200

    added myevents page

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :users do
-    get 'dashboard', to: "users#dashboard"
-  end
+  get "/dashboard", to: "pages#index"
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
