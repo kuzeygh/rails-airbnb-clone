732d9d4b25e180552858307251c37fbb4c34e54b
WladStan <wladharcourt@gmail.com>
2017-06-16 13:28:43 +0200

    add city to models and search bar

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ApplicationRecord
   validates :name, presence: true
   validates :category, presence: true
   validates :location, presence: true
+  validates :city, presence: true
   validates :event_time, presence: true
   validates :description, presence: true
 
