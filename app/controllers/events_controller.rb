f0d47e8c41a89defe2143b396f5337e0751dda62
Weronika Tomaszewska <vikariusu@gmail.com>
2017-06-16 13:02:38 +0200

    added attendances, some css

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,6 +24,7 @@ class EventsController < ApplicationController
   end
 
   def show
+
   end
 
   def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -8,4 +8,6 @@ class Event < ApplicationRecord
 
   mount_uploader :photo, PhotoUploader
   belongs_to :user
+  has_many :attendances
+  has_many :attenders, through: :attendances, source: :user
 end
